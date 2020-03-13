e4b84901287da1832223b88b33a6946f67cc9531
aniket2710 <aniket@cloudneeti.com>
2020-03-13 18:14:06 +0530

    updated the file names for AWS config

--- scripts/aws-config-onboarding/deploy-config.sh
+++ scripts/aws-config-onboarding/deploy-config.sh
@@ -21,11 +21,10 @@
       - Run this script in any bash shell (linux command prompt)
 
 .EXAMPLE
-    Command to execute : bash config-deployment.sh [-a <12-digit-account-id>] [-c <cloudformation-stack-name>] [-e <environment-prefix>] [-n <config-aggregator-name] [-p <primary-aggregator-region>] [-s <list of regions(secondary) where config is to enabled>]
+    Command to execute : bash deploy-config.sh [-a <12-digit-account-id>] [-e <environment-prefix>] [-n <config-aggregator-name] [-p <primary-aggregator-region>] [-s <list of regions(secondary) where config is to enabled>]
 
 .INPUTS
     (-a)Account Id: 12-digit AWS account Id of the account where you want the remediation framework to be deployed
-    (-c)Cloudformation Stack name: Name of the deployment stack that will be created
     (-e)Environment prefix: Enter any suitable prefix for your deployment
     (-n)Config Aggregator Name: Suitable name for the config aggregator
     (-p)Config Aggregator region(primary): Programmatic name of the region where the the primary config with an aggregator is to be created(eg:us-east-1)
@@ -37,17 +36,14 @@
     None
 '
 
-usage() { echo "Usage: $0 [-a <12-digit-account-id>] [-c <cloudformation-stack-name>] [-e <environment-prefix>] [-n <config-aggregator-name] [-p <primary-aggregator-region>] [-s <list of regions(secondary) where config is to enabled>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-a <12-digit-account-id>] [-e <environment-prefix>] [-n <config-aggregator-name] [-p <primary-aggregator-region>] [-s <list of regions(secondary) where config is to enabled>]" 1>&2; exit 1; }
 env="dev"
 version="1.0"
 regionlist=('na')
-while getopts "a:c:e:n:p:s:" o; do
+while getopts "a:e:n:p:s:" o; do
     case "${o}" in
         a)
             awsaccountid=${OPTARG}
-            ;;
-        c)
-            stack=${OPTARG}
             ;;
         e)
             env=${OPTARG}
@@ -73,9 +69,22 @@ if [[ "$awsaccountid" == "" ]] || ! [[ "$awsaccountid" =~ ^[0-9]+$ ]] || [[ ${#a
 fi
 
 env="$(echo "$env" | tr "[:upper:]" "[:lower:]")"
-stack="$(echo "$stack" | tr "[:upper:]" "[:lower:]")"
+aggregatorname="$(echo "$aggregatorname" | tr "[:upper:]" "[:lower:]")"
+aggregatorregion="$(echo "$aggregatorregion" | tr "[:upper:]" "[:lower:]")"
+regionlist="$(echo "$regionlist" | tr "[:upper:]" "[:lower:]")"
 
 aws_regions=( "na" "us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1" "ap-east-1" )
+
+echo "Verifying if the config aggregator or the config deployment bucket with the similar environment variable exists in the account..."
+s3_detail="$(aws s3api get-bucket-versioning --bucket config-bucket-$env-$awsaccountid 2>/dev/null)"
+s3_status=$?
+
+sleep 5
+
+if [[ $s3_status -eq 0 ]]; then
+    echo "Config bucket with name config-bucket-$env-$awsaccountid already exists in the account. Please verify if a cloudneeti aggregator already exists or re-run the script with different environment variable."
+    exit 1
+fi
 
 if [[ " ${aws_regions[*]} " != *" $aggregatorregion "* ]]; then
     usage
@@ -104,15 +113,23 @@ if [[ ${#validated_regions[@]} != ${#input_regions[@]} ]]; then
     usage
 fi
 
-aws cloudformation deploy --template-file deploy-config.yml --stack-name $env-$stack --region $aggregatorregion --parameter-overrides Stack=$env-$stack awsaccountid=$awsaccountid aggregatorname=$aggregatorname --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+aws cloudformation deploy --template-file config-aggregator.yml --stack-name "cn-data-collector-"$env --region $aggregatorregion --parameter-overrides env=$env awsaccountid=$awsaccountid aggregatorname=$aggregatorname --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
 aggregator_status=$?
 
 if [[ "$aggregator_status" -eq 0 ]] && [[ "${input_regions[0]}" != "na" ]]; then
     for region in "${input_regions[@]}"; do
         if [[ "$region" != "$aggregatorregion" ]]; then
-            aws cloudformation deploy --template-file multi-regionconfig.yml --stack-name $env-$stack --region $region --parameter-overrides Stack=$env-$stack awsaccountid=$awsaccountid aggregatorregion=$aggregatorregion --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+            aws cloudformation deploy --template-file multiregion-config.yml --stack-name "cn-data-collector-"$env --region $region --parameter-overrides env=$env awsaccountid=$awsaccountid aggregatorregion=$aggregatorregion --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+            multiregionconfig_status=$?
         fi
     done
+
+elif [[ "${input_regions[0]}" == "na" ]] || [[ "$multiregionconfig_status" -eq 0 ]]; then
+    echo "Successfully deployed config(s) and aggregator in the mentioned regions!!"
+
+elif [[ "${input_regions[0]}" == "na" ]]; then
+    echo "Successfully deployed config(s) and aggregator in the mentioned regions!!"
+
 else
     echo "Something went wrong! Please contact Cloudneeti support for more details"
 fi
