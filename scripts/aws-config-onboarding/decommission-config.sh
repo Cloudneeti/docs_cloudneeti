4c0f22d84304468b107760625a5f7fc5628c66e7
Aniket Gaikwad <aniket@cloudneeti.com>
2020-03-27 17:20:47 +0530

    exception handling and error message updates

--- scripts/aws-config-onboarding/decommission-config.sh
+++ scripts/aws-config-onboarding/decommission-config.sh
@@ -66,9 +66,9 @@ env="$(echo "$env" | tr "[:upper:]" "[:lower:]")"
 aggregatorregion="$(echo "$aggregatorregion" | tr "[:upper:]" "[:lower:]")"
 regionlist="$(echo "$regionlist" | tr "[:upper:]" "[:lower:]")"
 
-aws_regions=( "na" "us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1" "ap-east-1" )
+aws_regions=( "na" "us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
 
-if [[ " ${aws_regions[*]} " != *" $aggregatorregion "* ]]; then
+if [[ " ${aws_regions[*]} " != *" $aggregatorregion "* ]] || [[ " ${aggregatorregion} " != *" $aggregatorregion "* ]]; then
     usage
 fi
 
