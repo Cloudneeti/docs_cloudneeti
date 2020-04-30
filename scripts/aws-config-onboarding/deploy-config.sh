22fc1996d16da56a77b8be7cc16f50fda1d607c1
ankitsrao <ankitrao7739@gmail.com>
2020-04-30 15:17:13 +0530

    Confirmation message for user

--- scripts/aws-config-onboarding/deploy-config.sh
+++ scripts/aws-config-onboarding/deploy-config.sh
@@ -70,9 +70,18 @@ shift $((OPTIND-1))
 
 aws_regions=( "us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
 
+confugure_account="$(aws sts get-caller-identity)"
+
 if [[ "$awsaccountid" == "" ]] || ! [[ "$awsaccountid" =~ ^[0-9]+$ ]] || [[ ${#awsaccountid} != 12 ]] || [[ "$aggregatorname" == "" ]]; then
     usage
 fi
+
+if [[ "$confugure_account" != *"$awsaccountid"* ]];then
+    echo "AWS CLI configuration account and entered configuration account does not match. Please try again with correct AWS Account Id."
+    exit 1
+fi
+
+read -p "This script will delete any default config recorders and delivery channels present in entered regions of the AWS Account: $awsaccountid. Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 env="$(echo "$env" | tr "[:upper:]" "[:lower:]")"
 aggregatorname="$(echo "$aggregatorname" | tr "[:upper:]" "[:lower:]")"
