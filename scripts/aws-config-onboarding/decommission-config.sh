87b8b897969191b1225f3bc1bd1f6f5a184e402e
ankitsrao <ankitrao7739@gmail.com>
2020-10-01 12:29:18 +0530

    Config service-role update

--- scripts/aws-config-onboarding/decommission-config.sh
+++ scripts/aws-config-onboarding/decommission-config.sh
@@ -85,9 +85,9 @@ done
 
 aggregator_regions=( "us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1" "sa-east-1")
 
-confugure_account="$(aws sts get-caller-identity)"
+configure_account="$(aws sts get-caller-identity)"
 
-if [[ "$confugure_account" != *"$awsaccountid"* ]];then
+if [[ "$configure_account" != *"$awsaccountid"* ]];then
     echo "AWS CLI configuration AWS account Id and entered AWS account Id does not match. Please try again with correct AWS Account Id."
     exit 1
 fi
