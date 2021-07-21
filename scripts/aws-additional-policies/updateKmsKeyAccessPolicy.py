1dd4ae3af295749516a25a6efe2347416025b2a0
ankitsrao <ankitrao7739@gmail.com>
2021-07-21 11:35:49 +0530

    Updates as per review comments

--- scripts/aws-additional-policies/updateKmsKeyAccessPolicy.py
+++ scripts/aws-additional-policies/updateKmsKeyAccessPolicy.py
@@ -8,7 +8,7 @@ SYNOPSIS
 
 .NOTES
 
-  Copyright (c) Cloudneeti. All rights reserved.
+  Copyright (c) Zscaler. All rights reserved.
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -17,7 +17,7 @@ SYNOPSIS
 
 .EXAMPLE
     - Login to the AWS Management Console and navigate to AWS Cloudshell
-    - Upload this script on Cloudsheel using the Actions menu.
+    - Upload this script on Cloudshell using the Actions menu.
 
     Command to execute : python3 updateKmsKeyAccessPolicy.py --cspmRoleArn <CSPM Onboarding IAM Role ARN>
 
@@ -28,9 +28,9 @@ SYNOPSIS
     None
 '''
 
+import argparse
 import boto3
 import json
-import argparse
 
 def getAllRegions(servicename):
     regions=[]
